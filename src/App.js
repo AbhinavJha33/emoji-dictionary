bbe0d6e16e9312342735afaf853b4bcc3cbe4acc
AbhinavJha33 <91423026+AbhinavJha33@users.noreply.github.com>
2023-11-13 22:56:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import "./styles.css";
 
 export default function App() {
+  const [meaning, setMeaning] = useState("");
+  var emojiDictionary = {
+    "😀" : "Grinning Face",
+    "😂" : "Face with Tears of Joy"
+  };
+
+  var emojiArray = Object.keys(emojiDictionary);
+
+  function inputChangeHandler(event) {
+    var meaning = emojiDictionary[event.target.value];
+    if (meaning===undefined) {
+      meaning = "Sorry! This emoji is not available in our dictionary."
+    }
+    setMeaning(meaning);    
+  }
+
+  function emojiClickHandler(item) {
+    var meaning = emojiDictionary[event.target.value];
+    if (meaning===undefined) {
+      meaning = "Sorry! This emoji is not available in our dictionary."
+    }
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
       <h1>Emoji Dictionary</h1>
-      <input></input>
-      {/* <h2>Start editing to see some magic happen!</h2> */}
+      <input onChange= {inputChangeHandler}></input>
+      <div>{meaning}</div>
+      <div>emojis we know</div>
+      <div>{
+        emojiArray.map(item => 
+        <span key={item} >{item}
+        </span>)
+      }</div>
+                       
     </div>
   );
 }
