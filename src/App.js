97ecb85da8a466c6a8d99143c50bb2f5f6412703
AbhinavJha33 <91423026+AbhinavJha33@users.noreply.github.com>
2023-11-13 22:06:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Emoji Dictionary</h1>
+      <input></input>
+      {/* <h2>Start editing to see some magic happen!</h2> */}
+    </div>
+  );
+}
