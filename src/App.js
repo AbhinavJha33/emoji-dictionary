165a3e281e06d88e0adf133a8de1aa564deb3acd
AbhinavJha33 <91423026+AbhinavJha33@users.noreply.github.com>
2023-11-14 20:27:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [meaning, setMeaning] = useState("");
+  const [meaning, setMeaning] = useState("Emoji meaning will appear here");
   var emojiDictionary = {
-    "😀" : "Grinning Face",
-    "😂" : "Face with Tears of Joy"
+    "🙂": "smiling",
+    "😳": "disbelief",
+    "😔": "sad",
+    "😑": "annoyance",
+    "😃": "Grinning Face with Big Eyes",
+    "😄": "Grinning Face with Smiling Eyes",
+    "😁": " Beaming Face with Smiling Eyes",
+    "😆": " Grinning Squinting Face",
+    "😅": " Grinning Face with Sweat",
+    "🤣": " Rolling on the Floor Laughing",
+    "😂": " Face with Tears of Joy",
+    "🙃": " Upside-Down Face",
+    "😉": " Winking Face",
+    "😊": " Smiling Face with Smiling Eyes",
+    "😇": " Smiling Face with Halo",
+    "🥰": " Smiling Face with Hearts",
+    "😍": " Smiling Face with Heart-Eyes",
+    "🤩": " Star-Struck",
+    "😘": " Face Blowing a Kiss",
+    "😗": " Kissing Face",
+    "😚": " Kissing Face with Closed Eyes",
+    "😙": " Kissing Face with Smiling Eyes",
+    "😭": " Loudly Crying Face",
+    "😱": " Face Screaming in Fear",
+    "😖": " Confounded Face",
+    "😣": " Persevering Face",
+    "😞": " Disappointed Face",
+    "😓": " Downcast Face with Sweat",
+    "😩": " Weary Face",
+    "😫": " Tired Face",
+    "🥱": " Yawning Face",
+    "😤": " Face with Steam From Nose"
   };
 
   var emojiArray = Object.keys(emojiDictionary);
 
   function inputChangeHandler(event) {
     var meaning = emojiDictionary[event.target.value];
-    if (meaning===undefined) {
-      meaning = "Sorry! This emoji is not available in our dictionary."
+    if (meaning === undefined) {
+      meaning = "Sorry! This emoji is not available in our dictionary.";
     }
-    setMeaning(meaning);    
+    setMeaning(meaning);
   }
 
   function emojiClickHandler(item) {
     var meaning = emojiDictionary[item];
-    if (meaning===undefined) {
-      meaning = "Sorry! This emoji is not available in our dictionary."
+    if (meaning === undefined) {
+      meaning = "Sorry! This emoji is not available in our dictionary.";
     }
     setMeaning(meaning);
   }
@@ -28,15 +59,15 @@ export default function App() {
   return (
     <div className="App">
       <h1>Emoji Dictionary</h1>
-      <input onChange= {inputChangeHandler}></input>
-      <div>{meaning}</div>
-      <div>emojis we know</div>
-      <div>{
-        emojiArray.map(item => 
-        <span key={item} >{item}
-        </span>)
-      }</div>
-                       
+      <input onChange={inputChangeHandler}></input>
+      <div id="emojiMeaning">{meaning}</div>
+      <div>
+        {emojiArray.map((item) => (
+          <span id="emojis" key={item} onClick={() => emojiClickHandler(item)}>
+            {item}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
